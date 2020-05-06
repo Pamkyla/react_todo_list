ca59fbcf6fd6eb257dd31bb25d21021f8bb4e244
unknown <pamkylka@gmail.com>
2020-05-06 22:02:33 +0300

    Start styling and start second refactoring app

--- src/components/TodoListItem.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-const TodoListItem = () => {
-    return <span>Learn HTML</span>;
-}
-
-export default TodoListItem;
--- src/index.js
+++ src/index.js
@@ -4,17 +4,25 @@ import ReactDOM from 'react-dom';
  // Aтрибуты называются верблюдом
   // Атрибут class = className
   // Атрибут for = htmlFor
+  // key for proizvoditel'nocti, а т.е. React узнает какие элементы измениились(чтобы изменить ТОЛЬКО это)
 
 import AppHeader from './components/AppHeader';
-import SearchBlock from './components/SearchBlock';
-import TodoList from './components/TodoList';
+import SearchBlock from './components/SearchBlock/SearchBlock';
+import TodoList from './components/TodoList/TodoList';
 
 const App = () => {
+
+  const todoData = [
+    { label: 'Learn HTML', important: true, id: 1 },
+    { label: 'Learn JS', important: true, id: 2 },
+    { label: 'Learn React', important: false, id: 3 }
+  ];
+
   return (
   <div>
     <AppHeader/>
     <SearchBlock/>
-    <TodoList/>
+    <TodoList todoItems = {todoData}/>
   </div>
   );
 }
