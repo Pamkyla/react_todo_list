f521e697f8fc1df3f796b526857c678b634a1496
unknown <pamkylka@gmail.com>
2020-05-12 11:29:45 +0300

    repeat

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import './AppHeader.css';
 
 const AppHeader = () => {
     return <h1>Todo List</h1>;
---
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './ItemStatusFilter.css';
+
+class ItemStatusFilter extends React.Component{
+    render() {
+        return (
+            <div className='btn-group ItemStatusFilter'>
+                <button className='btn btn-success'>All</button>
+                <button className='btn btn-outline-secondary'>Active</button>
+                <button className='btn btn-outline-secondary'>Done</button>
+            </div>      
+        );
+    };
+    }
+
+    
+/* const ItemStatusFilter = () => {
+    return (
+        <div className='btn-group'>
+            <button className='btn btn-success'>All</button>
+            <button className='btn btn-outline-secondary'>Active</button>
+            <button className='btn btn-outline-secondary'>Done</button>
+        </div>      
+    );
+}; */
+
+export default ItemStatusFilter;
