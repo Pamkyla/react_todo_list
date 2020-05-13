4c5d69c4b32874e4256aad029890be18d73a586f
unknown <pamkylka@gmail.com>
2020-05-13 22:01:25 +0300

    onImportant onDone and onDelete(in process)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,21 +5,40 @@ import SearchBlock from '../SearchBlock';
 import TodoList from '../TodoList';
 import './App.css'
 
-const App = () => {
-
-  const todoData = [
+class App extends React.Component {
+state = {
+  todoData: [
     { label: 'Learn HTML', important: true, id: 1 },
     { label: 'Learn JS', important: true, id: 2 },
     { label: 'Learn React', important: false, id: 3 }
-  ];
+  ]
+};
 
-  return (
-  <div className="App">
-    <AppHeader/>
-    <SearchBlock/>
-    <TodoList todoItems = {todoData}/>
-  </div>
-  );
+deleteItem = (id) => {
+  this.setState(({ todoData }) => {
+    const index = todoData.findIndex((elem) => elem.id === id);
+    todoData.splice(index, 1);
+    return{
+      todoData: todoData 
+    }
+  });
 }
+
+  render() {
+    return (
+      <div className="App">
+        <AppHeader/>
+        <SearchBlock/>
+        <TodoList todoItems = {this.state.todoData} onDelete={ this.deleteItem }/>
+      </div>
+      );
+  }
+}
+
+
+
+ 
+  
+
 
 export default App;
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import TodoListItem from '../TodoListItem';
 import './TodoList.css';
-const TodoList = ( {todoItems} ) => {
+const TodoList = ( {todoItems, onDelete} ) => {
 
   const elements = todoItems.map((element) => {
     const { id, ...otherProps } = element;
     return (
-      <li key={id} className="list-group-item"><TodoListItem {...otherProps} />
+      <li key={id} className="list-group-item">
+        <TodoListItem 
+          {...otherProps} 
+          onDelete={() => onDelete(id)}
+        
+        />
         </li>
     );
   });
